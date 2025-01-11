2ec294b2bea4755741521c51a0a7f126ed938c37
Daniel Quevedo <danielelvisquevedo@gmail.com>
2025-01-11 15:06:59 -0300

    feat: Add main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+def binary_search(arr, low, high, x):
+
+    while low <= high:
+
+        mid = low + (high - low) // 2
+
+        if arr[mid] == x:
+            return mid
+
+        elif arr[mid] < x:
+            low = mid + 1
+
+        elif arr[mid] > x:
+            high = mid - 1
+
+
+if __name__ == '__main__':
+    arr = [2, 3, 4, 10, 40]
+    x = 2
+
+    result = binary_search(arr, 0, len(arr)-1, x)
+    if result != -1:
+        print("Element is present at index", result)
+    else:
+        print("Element is not present in array")
